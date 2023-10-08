b77c5528b749203c95c139c9c39691b3436b1f4b
Samuel Campelo <samuelcampelo25@gmail.com>
2023-10-07 21:18:48 -0300

    Cria funções para o arquivo.

--- Python/Gerenciador_de_senhas/Arquivo/__init__.py
+++ Python/Gerenciador_de_senhas/Arquivo/__init__.py
@@ -1,10 +1,34 @@
-def confirmaSeArquivoExiste(nome):
+def confirmarSeArquivoExiste(nomeDoArquivo):
     try:
-        arquivo = open(nome, 'rt')
+        arquivo = open(nomeDoArquivo, 'rt')
         arquivo.close()
-    
+        
     except FileNotFoundError:
         return False
     
     else:
         return True
+
+
+def criarArquivo(nomeDoArquivo):
+    arquivo = open(nomeDoArquivo, 'wt+')
+    arquivo.close()
+
+
+def mostrarArquivo(nomeDoArquivo):
+    arquivo = open(nomeDoArquivo, 'rt', encoding = 'utf-8')
+
+    print(f'{"Plataforma:":<35} {"Senha:"}') 
+    
+    for linha in arquivo:
+        plataformaESenha = linha.split(';')
+        
+        print(f'{plataformaESenha[0]:<35} {plataformaESenha[1]}'.replace('\n', ''))
+
+
+def cadastrarSenha(nomeDoArquivo, nomeDaPlataforma = 'Desconhecido', senha = 'Desconhecida'):
+    arquivo = open(nomeDoArquivo, 'at')
+    
+    arquivo.write(f'{nomeDaPlataforma};{senha}\n')
+    
+    print(f'Senha de {nomeDaPlataforma} cadastrada!')
