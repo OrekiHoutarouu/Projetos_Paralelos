e7da39f01e6a32a62dccd6d5f0077bb012bf92ae
Samuel Campelo <samuelcampelo25@gmail.com>
2023-10-16 22:03:55 -0300

    Cria arquivo .exe.

--- Python/Gerenciador_de_contas/venv/Scripts/activate.bat
+++ Python/Gerenciador_de_contas/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Samuel Campelo\Documents\GitHub\Side-Projects\Python\Gerenciador_de_senhas\venv
+set VIRTUAL_ENV=C:\Users\Samuel Campelo\Documents\GitHub\Side-Projects\Python\Gerenciador_de_contas\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
