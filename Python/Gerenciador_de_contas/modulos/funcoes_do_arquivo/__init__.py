518b26ed29497e55c05533508a32f312327f1c23
Samuel Campelo <samuelcampelo25@gmail.com>
2023-10-18 21:55:11 -0300

    Melhora funções e diminui 12 linhas.

--- Python/Gerenciador_de_contas/modulos/funcoes_do_arquivo/__init__.py
+++ Python/Gerenciador_de_contas/modulos/funcoes_do_arquivo/__init__.py
@@ -43,16 +43,19 @@ def verContasCadastradas(caminho):
     numeroDeArquivos = utilidades.contarArquivos(caminho)
     
     if numeroDeArquivos == -1:
-        print('Para ver as contas cadastradas é necessário cadastrar uma conta (Opção 1).')
-
-    for indice, conta in enumerate(os.listdir(caminho)):  
-        with open(f'{caminho}/{conta}', 'r', encoding = 'utf-8') as file:
-            informacao = json.load(file) 
-                  
-            print(f'{indice + 1} ', end='')
-            print(f'{informacao["plataforma"]:<34}', end='')
-            print(f'{informacao["email"]:<41}', end='')
-            print(f'{informacao["senha"]}')
+        print('Para excluir visualizar ou excluir alguma conta é necessário primeiro cadastrar uma conta (Opção 1).')
+        
+    else:
+        for indice, conta in enumerate(os.listdir(caminho)):  
+            with open(f'{caminho}/{conta}', 'r', encoding = 'utf-8') as file:
+                informacao = json.load(file) 
+                    
+                print(f'{indice + 1} ', end='')
+                print(f'{informacao["plataforma"]:<34}', end='')
+                print(f'{informacao["email"]:<41}', end='')
+                print(f'{informacao["senha"]}')
+                
+        print('-'*100)
             
             
 def excluirConta(caminho):
@@ -62,41 +65,26 @@ def excluirConta(caminho):
         caminho (str): É o caminho em que a conta que será excluida se encontra
     """
     
-    print(f'{"  Plataformas:":<35} {"E-mails:":<40} {"Senhas:"}')
-    print('-'*100)
-    
     numeroDeArquivos = utilidades.contarArquivos(caminho)
+    verContasCadastradas(caminho)
     
-    if numeroDeArquivos == -1:
-        print('Para excluir alguma conta é necessário cadastrar uma conta (Opção 1).')
+    if numeroDeArquivos != -1:
+        while True:
+            try:
+                qualContaExcluir = int(input('Índice da conta que deseja excluir: ')) - 1 
 
-    for indice, conta in enumerate(os.listdir(caminho)):  
-        with open(f'{caminho}/{conta}', 'r', encoding = 'utf-8') as file:
-            informacao = json.load(file) 
-                  
-            print(f'{indice + 1} ', end='')
-            print(f'{informacao["plataforma"]:<34}', end='')
-            print(f'{informacao["email"]:<41}', end='')
-            print(f'{informacao["senha"]}')
+                if qualContaExcluir > numeroDeArquivos or qualContaExcluir < 0:
+                    raise ValueError
+                
+            except ValueError:
+                print('Digite uma opção válida.')
             
-    print('-'*100)
-
-    
-    while True:
-        try:
-            qualContaExcluir = int(input('Índice da conta que deseja excluir: ')) - 1 
-
-            if qualContaExcluir > numeroDeArquivos or qualContaExcluir < 0:
-                raise ValueError
-            
-        except ValueError:
-            print('Digite uma opção válida.')
-        
-        else:
-            for indice, conta in enumerate(os.listdir(caminho)):
-                if indice == qualContaExcluir:
-                    os.remove(f'{caminho}/{conta}')
-                    print(f'Conta de {conta["plataforma"]} excluída com êxito.')
-                    
-                    break
-            break
+            else:
+                for indice, conta in enumerate(os.listdir(caminho)):
+                    if indice == qualContaExcluir:
+                        os.remove(f'{caminho}/{conta}')
+                        
+                        print(f'Conta de {conta[:-5]} excluída com êxito.')
+                        
+                        break
+                break
