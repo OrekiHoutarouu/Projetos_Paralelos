e31d78db0884f564149148c8f907bbcf0187570b
Samuel Campelo <samuelcampelo25@gmail.com>
2023-10-22 23:45:14 -0300

    Cria funções sobre senha.
    Cria função de sugere senha forte e cria protótipo de analisador de senha forte.

--- Python/Gerenciador_de_contas/Principal.py
+++ Python/Gerenciador_de_contas/Principal.py
@@ -1,3 +1,4 @@
+from re import A
 from modulos import funcoes_do_arquivo, utilidades
 import os
 from time import sleep
@@ -13,13 +14,15 @@ while True:
     print('1 - Cadastrar nova conta')
     print('2 - Ver contas cadastradas')
     print('3 - Deletar alguma conta')
-    print('4 - Sair do programa')
+    print('4 - Analisar se sua senha é forte')
+    print('5 - Sugerir senha forte')
+    print('6 - Sair do programa')
     
     while True:
         try:
             opcao = int(input('Opção: '))
             
-            if opcao > 4 or opcao < 1:
+            if opcao > 6 or opcao < 1:
                 print('Digite uma opção válida.')
                 
             else:
@@ -28,42 +31,57 @@ while True:
         except ValueError:
             print('Digite uma opção válida')
                 
-                
-    if opcao == 1:
-        print('='*100)
-        print('CADASTRAR NOVA CONTA'.center(100))
-        print('='*100)
-        
-        funcoes_do_arquivo.cadastrarContas(caminho, conta = {
-                'plataforma': str(input('Plataforma: ')),
-                'email': str(input('E-mail: ')),
-                'senha': str(input('Senha: '))
-                }
-        )
-        
-        sleep(1.5)
-    
-    elif opcao == 2:
-        print('='*100)
-        print('VER CONTAS CADASTRADAS'.center(100))
-        print('='*100)
-        
-        funcoes_do_arquivo.verContasCadastradas(caminho)
-        sleep(2)
+    match opcao:
+        case 1:
+            print('='*100)
+            print('CADASTRAR NOVA CONTA'.center(100))
+            print('='*100)
             
-    elif opcao == 3:
-        print('='*100)
-        print('EXCLUIR CONTA'.center(100))
-        print('='*100)
+            funcoes_do_arquivo.cadastrarContas(caminho, conta = {
+                    'plataforma': str(input('Plataforma: ')),
+                    'email': str(input('E-mail: ')),
+                    'senha': str(input('Senha: '))
+                    }
+            )
+            
+            sleep(1.5)
         
-        funcoes_do_arquivo.excluirConta(caminho)
-        sleep(2)
-        
-    elif opcao == 4:
-        print('='*100)
-        print('FINALIZANDO PROGRAMA'.center(100))
-        print('='*100)
-        
-        sleep(2)
-        
-        break
+        case 2:
+            print('='*100)
+            print('VER CONTAS CADASTRADAS'.center(100))
+            print('='*100)
+            
+            funcoes_do_arquivo.verContasCadastradas(caminho)
+            sleep(2)
+                
+        case 3:
+            print('='*100)
+            print('EXCLUIR CONTA'.center(100))
+            print('='*100)
+            
+            funcoes_do_arquivo.excluirConta(caminho)
+            sleep(2)
+
+        case 4:
+            print('='*100)
+            print('ANALISAR SE SENHA É FORTE'.center(100))
+            print('='*100)
+
+            utilidades.analisarSenhaForte(str(input('Senha: ')))
+            
+        case 5:
+            print('='*100)
+            print('SUGERIR SENHA FORTE'.center(100))
+            print('='*100)
+            
+            utilidades.sugerirSenhaForte()
+            sleep(2)
+
+        case 6:
+            print('='*100)
+            print('FINALIZANDO PROGRAMA'.center(100))
+            print('='*100)
+            
+            sleep(2)
+            
+            break
--- Python/Gerenciador_de_contas/modulos/utilidades/__init__.py
+++ Python/Gerenciador_de_contas/modulos/utilidades/__init__.py
@@ -1,4 +1,6 @@
 from os import listdir
+from random import shuffle
+import re
 
 def contarArquivos(caminho):
     """Conta quantos arquivos .json estão presentes na pasta "contas"
@@ -13,3 +15,60 @@ def contarArquivos(caminho):
         numeroDeArquivos += 1
     
     return numeroDeArquivos
+
+
+def analisarSenhaForte(senha):
+    caracteres = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '123456789', '@#$%&.,:;></\?!']
+
+    requisitosParaSerUmaSenhaForte = 0
+
+    for caractereDaSenha in senha:
+        for caractere in caracteres[0]:
+            if caractereDaSenha in caractere:
+                requisitosParaSerUmaSenhaForte += 1
+                break
+        
+        for caractere in caracteres[1]:
+            if caractereDaSenha in caractere:
+                requisitosParaSerUmaSenhaForte += 1
+                break
+        
+        for caractere in caracteres[2]:
+            if caractereDaSenha in caractere:
+                requisitosParaSerUmaSenhaForte += 1
+                break
+        
+        for caractere in caracteres[3]:
+            if caractereDaSenha in caractere:
+                requisitosParaSerUmaSenhaForte += 1
+                break
+            
+
+    match requisitosParaSerUmaSenhaForte:
+        case 1:
+            print('Sua senha é fraca')
+        
+        case 2:
+            print('Sua senha está entre média e fraca')
+
+        case 3:
+            print('Sua senha é média')
+        
+        case 4:
+            print('Sua senha é forte')
+
+
+def sugerirSenhaForte():
+    caracteres = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '123456789', '@#$%&.,:;></\?!']
+
+    print('Senha forte: ', end='')
+
+    for caractere in caracteres:
+        caractere = list(caractere.strip())
+
+        shuffle(caractere)
+
+        resultado = ''.join(caractere)
+        print(resultado[:3], end='')
+
+    print()
